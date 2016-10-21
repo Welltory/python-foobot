6c1ae64ba022eb7f95a3f273025919dcc59fa93b
Alexander Sapronov <sapronov.alexander92@gmail.com>
2016-10-21 14:59:31 +0700

    Add refresh_token method

--- foobot/api.py
+++ foobot/api.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import requests
 import datetime
+
+import requests
+
+__all__ = [
+    'FoobotAPI',
+    'FoobotDataItem',
+]
+
+
+class RefreshTokenError(Exception):
+    pass
 
 
 class FoobotDataItem(object):
@@ -27,39 +37,81 @@ class FoobotDataItem(object):
 class FoobotAPI(object):
     headers = {}
 
-    def __init__(self, api_key, access_token, homehost, username):
-        self.FOOBOT_API_KEY = api_key
+    def __init__(self,
+                 api_key,
+                 access_token,
+                 homehost,
+                 username,
+                 client_key=None,
+                 client_secret=None,
+                 refresh_token=None):
+        self.api_key = api_key
         self.homehost = homehost
         self.username = username
         self.access_token = access_token
+        self.client_key = client_key
+        self.client_secret = client_secret
+        self.refresh_token = refresh_token
+
         self.headers['Authorization'] = 'bearer {}'.format(self.access_token)
-        self.headers['X-API-KEY-TOKEN'] = self.FOOBOT_API_KEY
+        self.headers['X-API-KEY-TOKEN'] = self.api_key
+
+    def _get(self, url):
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        return {}
+
+    def _post(self, url, data):
+        response = requests.post(url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        raise RefreshTokenError(response.content)
+
+    def _url(self, url):
+        return 'https://{}/{}'.format(self.homehost, url)
+
+    @property
+    def region_token(self):
+        return self._get(self._url('v2/user/me/home/'))
+
+    def refresh_token(self):
+        if not self.client_key:
+            raise ValueError("Client key is not valid")
+
+        if not self.client_secret:
+            raise ValueError("Client key is not valid")
+
+        if not self.refresh_token:
+            raise ValueError("Refresh token is not valid")
+
+        data = {
+            'client_id': self.client_key,
+            'client_secret': self.client_secret,
+            'grant_type': 'refresh_token',
+            'refresh_token': self.refresh_token,
+        }
+        return self._post(self._url('oauth2/token'), data)
 
     def get_devices(self):
-        url = 'https://{}/v2/owner/{}/device/'.format(
-            self.homehost,
-            self.username
-        )
-        r = requests.get(url, headers=self.headers)
-        if r.status_code == 200:
-            return r.json()
-        return {}
+        return self._get(
+            self._url('v2/owner/{}/device/'.format(self.username)))
 
     def get_data(self, start=None, end='last', average_by=3600):
         if start is None:
             start = 3600 * 24  # pragma: no cover
         data = []
         for dev in self.get_devices():
-            url = 'https://{}/v2/device/{}/datapoint/{}/{}/{}/'.format(
+            url = 'v2/device/{}/datapoint/{}/{}/{}/'.format(
                 self.homehost,
                 dev['uuid'],
                 start,
                 end,
                 average_by
             )
-            r = requests.get(url, headers=self.headers)
-            if r.status_code == 200:
-                data.append(r.json())
+            item = self._get(self._url(url))
+            if item:
+                data.append(item)
         return data
 
     def get_last_day_data(self, raw=False):
@@ -68,5 +120,6 @@ class FoobotAPI(object):
             return data
         else:
             for i in range(len(data)):
-                data[i]['datapoints'] = [FoobotDataItem(d) for d in data[i]['datapoints']]
+                data[i]['datapoints'] = [FoobotDataItem(d) for d in
+                                         data[i]['datapoints']]
             return data
